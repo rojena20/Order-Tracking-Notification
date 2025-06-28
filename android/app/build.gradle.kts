eb1cdd5248872d04809b00eef9595a1f1072f0bb
Rojena <rojenamohamaden@gmail.com>
2025-06-28 10:53:11 +0300

    edit gradel to prepare the project for firebase integration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,6 +16,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -49,4 +50,5 @@ flutter {
 
 dependencies {
     implementation("com.android.support:multidex:1.0.3")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
